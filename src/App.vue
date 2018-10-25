310659c7dee16faa251f4f10c427493d9c56f332
Xeltica <citringo@gmail.com>
2018-10-25 12:13:37 +0900

    パーティクルエフェクト実装

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
-<template>
-	<component :is="$store.state.signedIn ? 'Main' : 'Welcome'"/>
+<template lang="pug">
+#root
+	vue-particles(
+		color="#fafafa",
+		:particleOpacity="0.2",
+		:particlesNumber="80",
+		shapeType="circle",
+		:particleSize="4",
+		linesColor="#fafafa",
+		:linesWidth="1",
+		:lineLinked="true",
+		:lineOpacity="0.1",
+		:linesDistance="150",
+		:moveSpeed="4",
+		:hoverEffect="false",
+	)
+	component(:is="$store.state.signedIn ? 'Main' : 'Welcome'")
 </template>
 
 <script lang="ts">
@@ -14,9 +29,25 @@ import "normalize.css";
 	},
 })
 export default class App extends Vue {
+	public mounted() {
+		
+	}
 }
 </script>
 
 <style lang="scss">
 @import "./styles/global";
+
+#particles-js {
+	position:fixed;
+	left: 0;
+	right: 0;
+	top: 0;
+	bottom: 0;
+
+	&, &>canvas {
+		z-index: -100;
+	}
+}
+
 </style>
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
+import VueParticles from "vue-particles";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueParticles);
 
 new Vue({
   router,
