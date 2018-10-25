abbc7112938f548955eabdaccca4266d299283ca
Xeltica <citringo@gmail.com>
2018-10-25 10:48:14 +0900

    wip なページの説明を出すように

--- src/router.ts
+++ src/router.ts
@@ -18,21 +18,33 @@ export default new Router({
 			path: "/me",
 			name: "me",
 			component: Wip,
+			props: {
+				desc: "アカウントページでは、あなたのアカウント情報を詳しく確認できます。また、アカウント情報の編集をしたり、公開範囲のカスタマイズも行えます。",
+			}
 		},
 		{
 			path: "/drive",
 			name: "drive",
 			component: Wip,
+			props: {
+				desc: "Axella Driveでは、１人100GBのクラウドドライブを自由に使えます。ウェブフォルダーにファイルを入れるだけで、ウェブサイトの公開もできるので、ご自由にお使いください(php, cgiなどは動作しません)。",
+			}
 		},
 		{
 			path: "/download",
 			name: "download",
 			component: Wip,
+			props: {
+				desc: "ダウンロードページでは、Axellaが提供する素材などのデータをダウンロードできます。",
+			}
 		},
 		{
 			path: "/setting",
 			name: "setting",
 			component: Wip,
+			props: {
+				desc: "設定ページでは、ポータルのUIや、言語設定などを行えます。",
+			}
 		},
 	],
 });
--- src/views/Wip.vue
+++ src/views/Wip.vue
@@ -2,4 +2,15 @@
 #root
 	h1 Work in progress!
 	p この機能は現在、鋭意開発中です！しばしお待ち下さい。
+	p(v-if="desc") {{ desc }}
 </template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+import VMenu from "@/components/Menu.vue";
+
+@Component
+export default class Wip extends Vue {
+	@Prop() desc: string;
+}
+</script>
